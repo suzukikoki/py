4d572c20911daa7d7f0f28fbcd34d222cb870e13
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2023-01-29 12:46:00 +0000

    [

--- kyoto1.py
+++ kyoto1.py
@@ -196,3 +196,25 @@ b = 0
 if a == 1:
   if b==0:
     print("YES a==1 and b == 0")
+
+#端末からの入力
+# a = input("")
+# print(a)
+
+#エラー対処
+while True:
+  x = input("正の数値を入力してください")
+  try:
+    x = float(x)
+  except ValueError:
+    print(x, "は数値に変換できません")
+    continue
+  except:
+    print("予期していないエラーです")
+    exit()
+  if x <=0:
+    print(x, "は正の数値ではありません")
+    continue
+# 以下は正しい入力が得られた時の処理
+  print(x)
+
